310be66145ae73fcdbbcb36bc980f8a0aa6f682a
Shivam Jaiswal <33538527+Jshivam0110@users.noreply.github.com>
2020-09-05 11:56:22 +0530

    icon in title tab
    added icon in title bar

--- js/script.js
+++ js/script.js
@@ -8,13 +8,6 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
-// window.addEventListener("load", function () {
-//   document.querySelector(".preloader").classList.add("opacity-0");
-
-//   setTimeout(function () {
-//     document.querySelector(".preloader").style.display = "none";
-//   }, 1000);
-// });
 
 
 //Portfolio item Filter
